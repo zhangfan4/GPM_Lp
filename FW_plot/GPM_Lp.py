3efbdbe97ab80e6773a13f8bc600ed6059cb1402
Fan Zhang <zhangfan4@shanghaitech.edu.cn>
2021-01-20 16:58:43 +0800

    Update GPM_Lp.py

--- FW_plot/GPM_Lp.py
+++ FW_plot/GPM_Lp.py
@@ -1,155 +1,219 @@
-"""
-    This file is the implementation of the Projectin gradient method
-    See the Algorithm Framework for details
-
-"""
-
-
 import numpy as np
 import sys
 from numpy import linalg as LA
 import Lp_proj
 import simplex_RT
-<<<<<<< HEAD
-from auxiliary_projection import projection_onto_hyperplane, projection_onto_weighted_l1_norm_ball
-=======
-import inexact_proj
->>>>>>> f24af5513d4e374ca8984e0694265f8a0aaae32a
+from scipy.optimize import root_scalar
 
 
 def loss(A, x, y):
     """
-    the objective function value
     return 0.5 * ||Ax - y||_2^2
     """
     return 0.5 * LA.norm(A.dot(x) - y, 2) ** 2
 
 
-def gradient_of_loss(A, x, y):
+def hyperplane(y_proj_act, weights_act, gamma_k):
     """
-    the gradient of the objective function
-    return A^T(Ax - y)
+    Do projection onto a hyperplane
+    min 0.5 * ||x_sub - y_proj_act||_2^2
+    s.t. y_proj_act^T weights_act = r,
+    :parameter gamma_k: the radius of L1-ball
+    :parameter y_proj_act: the point to be projected
+    :returns: x_sub: the projection onto the hyperplane 
+              scalar3: x_sub = y_proj_act - scalar3 * weights_act
     """
-    return A.T.dot(A.dot(x) - y)
+    scalar1 = np.dot(weights_act, y_proj_act) - gamma_k
+    scalar2 = sum(weights_act ** 2)
+    try:
+        scalar3 = np.divide(scalar1, scalar2)
+    except ZeroDivisionError:
+        print("Error! - derivation zero for scalar2 =", scalar2)
+        sys.exit(1)
+    x_sub = y_proj_act - scalar3 * weights_act
+    return x_sub, scalar3
 
 
+def WeightedL1Projection(y, weights, radius):
+    """
+    Do projection onto a weighted L1-ball
+    min 0.5 * ||x_opt - y||_2^2
+    s.t. weights^T |y| = radius,
+    :parameter radius: the radius of the L1-ball
+    :parameter weights: the weight
+    :parameter y: the point to be projected
+    :return: x_opt: primal optimal 
+    """
+
+    y_sign = np.sign(y)
+    y_proj = y_sign * y
+    act_ind = range(len(y))
+    while True:
+        # calculate y_bar_act
+        y_proj_act = y_proj[act_ind]
+        weights_act = weights[act_ind]
+
+        x_sol_hyper, lamb = hyperplane(y_proj_act, weights_act, radius)
+        y_proj_act = np.maximum(x_sol_hyper, 0.0)
+        y_proj[act_ind] = y_proj_act
+
+        # update the active set
+        act_ind = y_proj > 0
+
+        inact_ind_cardinality = sum(x_sol_hyper < 0)
+        if inact_ind_cardinality == 0:
+            x_opt = y_proj * y_sign
+            break
+
+    return x_opt
 
 
-def GPM(A, x, y, step_size, p, radius):
+def GPM(A, x, y, alpha, p, radius):
     """
     Gradient Projection Method
     :parameter A: the measurement matrix
     :parameter x: the initial guess of the signal 
     :parameter y: the measurement of signal
-    :parameter step_size: the step-size of the GPM
+    :parameter alpha: the step-size of the GPM
     :parameter radius: the radius of the L1-ball
     :returns: x: output of the GPM, 
               iter_Lp: # the projection onto the Lp-norm ball, 
               iter_L1: # the projection onto the Weighed L1-norm ball
     """
 
-    precision = 1e-6              # tolerance of numerical precision
+    tol = 1e-6              # tolerance of numerical precision
     iter = 0                # the number of total iteration
     iter_L1 = 0           # the number of the call of projected gradient descent method
     iter_Lp = 0             # the number of the call of Frank-Wolfe method
-    stopping_tol = 1e-6     # the tolerance of stopping criteria
+    stopping_tol = 1e-10     # the tolerance of stopping criteria
 
     while True:
         iter += 1
-
-        # compute the gradient of objective function
-        grad = gradient_of_loss(A, x, y)    
+        grad = np.dot(A.T, A.dot(x) - y)    # compute the gradient of objective
+        # x_pnorm = LA.norm(x, p) ** p
 
         # Check whether the current point is on the boundary of the Lp-ball
-        if abs(LA.norm(x, p) ** p - radius) <= precision:
+        if abs(LA.norm(x, p) ** p - radius) <= tol:
             """
-            Case I: x_k is on the boundary of the Lp-ball, ||x_k||_p^p = r
+            Case I: x_k is on the boundary of the Lp-ball
             """
             iter_L1 += 1
             x_pre = x
-
-            # the point to be projected
-            z = x - step_size * grad
+            z = x - alpha * grad
 
             # Check z feasible or infeasible (necessary?)
-            if LA.norm(z, p) ** p <= radius + precision:  # CASE I.1: z is feasible, that is, z is in the lp ball
+            if abs(LA.norm(z, p) ** p - radius) <= tol or LA.norm(z, p) ** p <= radius:  # Will this situation happen?
+                # print('outer-outer')
                 x = z
 
-            else:                        # CASE I.2: z is infeasible, that is, z is outside the lp ball, we need do projection
+            else:
                 ''' Projecting z onto the weighed L1 norm ball '''
                 # print('outer-inner')
-
-                # keep the same active set between x and z
-                active_index = np.where(abs(x) > precision)[0]
+                # find the active set and calculate the weight
+                act_ind = np.where(abs(x) > tol)[0]
                 for i in range(len(z)):
-                    if i not in active_index:
+                    if i not in act_ind:
                         z[i] = 0
-
-                # weight vector
                 w = np.zeros(len(x))
-                w[active_index] = abs(x[active_index]) ** (p - 1)
-
-                # radius
-                radius_L1 = w[active_index].dot(abs(x[active_index]))
+                w[act_ind] = p * abs(x[act_ind]) ** (p - 1)
+                radius_L1 = w[act_ind].dot(abs(x[act_ind]))
                 # weighted L1 projection
-                x = projection_onto_weighted_l1_norm_ball(z, w, radius_L1)
+                x = WeightedL1Projection(z, w, radius_L1)
+
+                # # TODO: zero element encountered, Insight: select inactive indices
+                # signum = np.ones(len(act_ind))
+                # lam = 0
+                # x_proj, _ = simplex_RT.bisection(signum, lam, z[act_ind], radius_L1, w[act_ind])
+                # x = np.zeros_like(z)
+                # x[act_ind] = x_proj
 
 
             # Stopping criteria
-            proj_residual = LA.norm(x - x_pre, 2)   # ||x_new - x_old||_2
-            if proj_residual < stopping_tol:
+            proj_res = LA.norm(x - x_pre, 2)
+            # print('{:5d}    {}:   Obj = {:3.3f}   Res = {:4.3e}   #nonzero = {}'.format(iter, 'Boundary case',
+            #                                                             loss(A, x, y), proj_res, np.count_nonzero(x)))
+            # Termination criterion \|x_k - x_{k-1}\|_2 <= eps_1
+            if proj_res < stopping_tol:
                 break
 
-        elif LA.norm(x, p) ** p < radius - precision:
+        elif LA.norm(x, p) ** p < radius:
             """
-            Case II: x_k is inside the Lp-ball, ||x_k||_p^p < r
+            Case II: x_k is inside the Lp-ball
             """
             iter_Lp += 1
+            norm_grad = LA.norm(grad) ** 2
 
             # Termination criterion \|\nabla f(x_k)\|_2 <= eps_2
-            if LA.norm(grad, 2) < stopping_tol:
+            if norm_grad < stopping_tol:
                 break
 
-<<<<<<< HEAD
-            z = x - step_size * grad
-
-            if LA.norm(z, p) ** p <= radius + precision:    # CASE II.1: z is feasible, that is, z is in the lp ball
-                # print('inner-inner')
-                x = z
-
-            else:               # CASE II.2: z is infeasible, that is, z is outside the lp ball, we need do projection
-                dim = len(z)
-                x_init = x  # the initial points for the algorithm 1
-
-                # Generate epsilon according to x.
-                # see 2101.01350.pdf, section 5.1 for details
-                epsilon = 0.9 * (1. / dim * (radius - LA.norm(x, p) ** p)) ** (1. / p) * np.ones(dim)  
-                # ensure that the point is feasible.
-=======
             z = x - alpha * grad
             if (LA.norm(z, p) ** p - radius) <= tol:
-                
                 # print('inner-inner')
                 x = z
 
             elif LA.norm(z, p) ** p > radius:
                 ''' Projecting z onto the Lp norm ball '''
-                # print('inner-outer')
-
-#                 dim = len(z)
-#                 x_ini = x  # the initial points for the algorithm
-
-                # %% Generate epsilon according to x.
-#                 epsilon = 0.9 * (1. / dim * (radius - LA.norm(x, p) ** p)) ** (1. / p) * np.ones(
-#                     dim)  # ensure that the point is feasible.
-#                 x = Lp_proj.WeightLpBallProjection(dim, x_ini, z, p, radius, epsilon)
+                print('inner-outer')
+                # print('11'*30)
+                # print(LA.norm(x, p) ** p - radius)
+                # print(LA.norm(z, p) ** p - radius)
+                # print('11' * 30)
+                dim = len(z)
+                x_ini = x  # the initial points for the algorithm
                 
-                beta = inexact_proj.bisection(x, d, radius, p, tol)  # line search for the stepsize beta satisfying ||x+beta*d||_p^p = radius
-                x += beta * d
->>>>>>> f24af5513d4e374ca8984e0694265f8a0aaae32a
+                '''Three ways to initialize epsilon'''
+                # 1. Generate epsilon according to x.
+                epsilon = 0.9 * (1. / dim * (radius - LA.norm(x, p) ** p)) ** (1. / p) * np.ones(dim)  # ensure that the point is feasible.
 
-                x = Lp_proj.WeightLpBallProjection(dim, x_init, z, p, radius, epsilon)
-                
+                # 2. Root finding for epsilon on the boundary
+                # low = 0
+                # up = (radius / dim) ** (1./p)
+                # tol_bi = 1e-10
+                # print('LA.norm(x, p) ** p - radius:', LA.norm(x, p) ** p - radius)
+                #
+                # def fun_eval(eps):
+                #     """
+                #         Function evaluation
+                #         f(lam) = sum_i w_i max(y_i - lam w_i, 0) - gamma
+                #     """
+                #     return LA.norm(abs(x)+eps, p) ** p - radius
+                # sol = root_scalar(fun_eval, method='toms748', bracket=[low, up], xtol=tol_bi)
+                # epsi = sol.root
+                # print('epsi:', epsi)
+                # epsilon = 0.9 * epsi * np.ones(dim)
+
+                # 3. Randomly generating epsilon.
+                # radius_rest = radius - LA.norm(x_ini, p) ** p
+                # rand_num = np.random.uniform(0, 1, dim)
+                # abs_norm = LA.norm(rand_num, ord=1)
+                # epsilon = 0.9 * (rand_num * radius_rest / abs_norm)**p  # ensure that the point is feasible.
+                # print('initial residual:', radius_rest)
+
+                # if max(epsilon) <= 1e-8:
+                #     print('simplex projection')
+                #     act_ind = np.where(abs(x) > tol)[0]
+                #     for i in range(len(z)):
+                #         if i not in act_ind:
+                #             z[i] = 0
+                #     w = np.zeros(len(x))
+                #     w[act_ind] = p * abs(x[act_ind]) ** (p - 1)
+                #     radius_L1 = w[act_ind].dot(abs(x[act_ind]))
+                #     # weighted L1 projection
+                #     x = WeightedL1Projection(z, w, radius_L1)
+                # else:
+                print('Lp ball projection')
+                # print(epsilon)
+                x, save_flag = Lp_proj.WeightLpBallProjection(dim, x_ini, z, p, radius, epsilon)
+                if save_flag is True:
+                    np.save('failed2/x0.npy', x_ini)
+                    np.save('failed2/y.npy', z)
+                    np.save('failed2/radius.npy', radius)
+                    exit()
+
+            # print('{:5d}    {}:   Obj = {:3.3f}   Res = {:4.3e}   #nonzero = {}'.format(iter, 'Interior case',
+            #                                                                             loss(A, x, y), norm_grad,
+            #                                                                             np.count_nonzero(x)))
         else:
             print('The new iterate is infeasible!')
 
